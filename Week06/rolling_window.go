688a49eb561d5c29b43928704e6668bc24557ed3
Zongwei Fang <zongwei.fang@shopee.com>
2021-01-07 01:48:56 +0800

    week06

--- Week06/rolling_window.go
+++ Week06/rolling_window.go
@@ -8,7 +8,7 @@ import (
 //滑动窗口，默认粒度为1s
 type RollingWindow struct {
 	Buckets map[int64]*numberBucket
-	Mutex   *sync.Mutex
+	Mutex   *sync.RWMutex
 	//滑动窗口长度
 	Size int64
 }
@@ -20,7 +20,7 @@ type numberBucket struct {
 func NewRollingWindow(size int64) *RollingWindow {
 	r := &RollingWindow{
 		Buckets: make(map[int64]*numberBucket),
-		Mutex:   &sync.Mutex{},
+		Mutex:   &sync.RWMutex{},
 		Size:    size,
 	}
 
@@ -59,4 +59,39 @@ func (rw *RollingWindow) removeBuckets() {
 			delete(rw.Buckets, timestamp)
 		}
 	}
+}
+
+func (rw *RollingWindow) Sum(now time.Time) float64 {
+	sum := float64(0)
+	rw.Mutex.RLock()
+	defer rw.Mutex.RUnlock()
+
+	for k, v := range rw.Buckets {
+		if k >= now.Unix() {
+			sum += v.Value
+		}
+	}
+
+	return sum
+}
+
+func (rw *RollingWindow) Avg(now time.Time) float64 {
+	return rw.Sum(now) / float64(rw.Size)
+}
+
+func (rw *RollingWindow) Max(now time.Time) float64 {
+	var max float64
+
+	rw.Mutex.RLock()
+	defer rw.Mutex.RUnlock()
+
+	for i, bucket := range rw.Buckets {
+		if i >= now.Unix()-rw.Size {
+			if bucket.Value > max {
+				max = bucket.Value
+			}
+		}
+	}
+
+	return max
 }
